dea99b23b6e8770001ec9f8c80ef68cc0d7b5e5b
ratchek <git@ratchek.com>
2022-07-19 17:50:39 -0400

    Create route for getting cals

--- dont-break-the-chain-backend/app/app.py
+++ dont-break-the-chain-backend/app/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
-
+import json
 app = Flask(__name__)
+
+temp_data = {"2021": {"1": [1,3,5], "3":[1,4,14]}}
+j = json.dumps(temp_data)
 
 @app.route('/')
 def index():
-    return 'Hi world!'
+    return 'This is my api. There are many like it, but this one is mine.'
+
+@app.route('/cal')
+def get_cal():
+    return j    
+
 
 if __name__ == "__main__":
     app.run()
