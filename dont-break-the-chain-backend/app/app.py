ed126d03efef06c580ad47d69e6a2153876424c0
ratchek <git@ratchek.com>
2022-07-20 16:59:12 -0400

    Add save method (for syncing)
    The save endpoint allows you to overwrite the whole calendar
    with one that you pass through JSON.
    
    Its there because I was worried that some requests might get lost
    in transit and then the frontend state  wouldn't be in sync with the
    backend state. So this can be used when you need to make sure that
    what you're seeing in the frontend is reflected on the backend.
    
    The other option was to request the whole calendar after any change
    on the frontend, but I felt that would be too unresponsive.

--- dont-break-the-chain-backend/app/app.py
+++ dont-break-the-chain-backend/app/app.py
@@ -1,18 +1,47 @@
-from flask import Flask
+from flask import Flask, request
 import json
 app = Flask(__name__)
 
-temp_data = {"2021": {"1": [1,3,5], "3":[1,4,14]}}
-j = json.dumps(temp_data)
+temp_data = {"2021": {"1": ["1","3","5"], "3":["1","4","14"]}}
+def print_data():
+    return json.dumps(temp_data)
+def set_data(data):
+    global temp_data
+    temp_data = data
 
 @app.route('/')
 def index():
     return 'This is my api. There are many like it, but this one is mine.'
 
-@app.route('/cal')
+@app.route('/cal', methods = ['POST', 'GET'])
 def get_cal():
-    return j    
+    if request.method == 'GET':
+        return print_data()
+    if request.method == 'POST':
+        day = request.json['day'] 
+        month = request.json['month'] 
+        year = request.json['year'] 
+        if year in temp_data:
+            if month in temp_data[year]:
+                if day in temp_data[year][month]:
+                    temp_data[year][month].remove(day)
+                else:
+                    temp_data[year][month].append(day)      
+            else: 
+                temp_data[year][month] = [day]
+        else:
+            temp_data[year] = {month: [day]}
+    return "Success!"
 
+# Overwrites the whole calendar with whatever the React page is sending over
+@app.route('/cal/save', methods = ['POST'])
+def save():
+    cal = request.json
+    set_data(cal)
+    print(cal)
+    print(print_data())
+    return "Saved?" 
+        
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
